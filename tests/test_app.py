6048628fd573b2f2ac687a93ddb439f79e4a8a53
Nikole647 <nikole647@gmail.com>
2023-08-23 13:46:45 +0300

    adding tests
    Signed-off-by: Nikole647 <nikole647@gmail.com>

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,32 @@
+
 import requests
 
-def test_ping_server():
-    # change the test
-    url = 'http://172.28.1.3:5000/'
+# def test_ping_server():
+#     # change the test
+#     url = 'http://172.28.1.3:5000/'
+#
+#     # Send a GET request to the server
+#     response = requests.get(url)
+#
+#     # Check if the response status code is 200 (OK)
+#     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
 
-    # Send a GET request to the server
-    response = requests.get(url)
+import pytest
+from flask_testing import TestCase
+from Server.Server import app
 
-    # Check if the response status code is 200 (OK)
-    assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
+class TestApp(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
 
+    def test_index(self):
+        response = self.client.get('/')
+        self.assert_template_used('index.html')
+        self.assert200(response)  # Check if the response is successful (status code 200)
 
+if __name__ == '__main__':
+    pytest.main()
 
 
 
