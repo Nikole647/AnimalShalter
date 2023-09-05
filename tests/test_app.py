c7d27f32575cc1003efa5b86d00e694a2ae673ab
Nikole647 <nikole647@gmail.com>
2023-09-05 18:19:32 +0300

    changing some files
    Signed-off-by: Nikole647 <nikole647@gmail.com>

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,4 @@
 
-#import requests
-
-# def test_ping_server():
-#     # change the test
-#     url = 'http://172.28.1.3:5000/'
-#
-#     # Send a GET request to the server
-#     response = requests.get(url)
-#
-#     # Check if the response status code is 200 (OK)
-#     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
-
 import pytest
 from flask_testing import TestCase
 from Server.Server import app
